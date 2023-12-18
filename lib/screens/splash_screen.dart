665ecf91dab928dc79ff27faa227bfad78db7e2a
Aya_djihane <aichouneaya@gmail.com>
2023-12-18 22:04:01 +0100

    Splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:myhb_app/widgets/custom_button.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var media= MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: media.width,
+            height: media.height,
+            decoration: const BoxDecoration(
+                image:DecorationImage(image: AssetImage("images/SplashBG.png"),fit: BoxFit.fill
+
+                )
+            ),
+          ),
+          Positioned(
+              bottom: 20,
+              left: media.width/3,
+              child: CustomButton(width: media.width/3,value: "Get Started",)),
+        ],
+      ),
+    );
+  }
+}
