43fd0006abd29b1789b710be387390fe0cbe5f4c
Aya_djihane <aichouneaya@gmail.com>
2023-12-18 23:25:18 +0100

    Firebase Initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 
-void main() =>runApp(MyApp());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
   @override
